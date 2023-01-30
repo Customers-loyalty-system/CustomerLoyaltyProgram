be509f5423b9a902af37a7534f42d030e52295aa
ubaeida <ubaeida.alkayal@gmail.com>
2023-01-30 15:36:38 +0300

    activity model done

--- migrations/20230125074937-create-activity.js
+++ migrations/20230125074937-create-activity.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Activiteys", {
+    await queryInterface.createTable("Activitys", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Activiteys");
+    await queryInterface.dropTable("Activitys");
   },
 };
